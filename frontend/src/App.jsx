b753909d64d7f4bdba49c8bec3bd630ba5ecc665
Daniel Steven <40723614+dan-p-steven@users.noreply.github.com>
2026-02-02 22:00:28 -0500

    Revert "Feature/fe 7 form components 1.1"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar.jsx"
 import { Routes, Route} from "react-router-dom"
 import Login from "./routes/Login"
 import Profile from "./routes/Profile"
-import ProfileEdit from './routes/ProfileEdit.jsx'
 
 // npm run dev http://localhost:5173/ 
 
@@ -16,7 +15,6 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login/>} />
             <Route path="/profile" element={<Profile/>} />
-            <Route path="/profile_editing" element={<ProfileEdit/>} />
           </Routes>
       </div>
     </>
--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,53 +1,10 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useNavigate } from "react-router-dom";
 
-const Login = ( { setUser }) => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [emailError, setEmailError] = useState("")
-    const [pwdError, setPwdError] = useState("")
-    const [loginMsg, setloginMsg] = useState("")
-    const navigate = useNavigate()
-
+const Login = () => {
     return (
-        <div className="Login-layout"> {/* For login box layout */}
-            {loginMsg && <div className="login-msg">{loginMsg}</div>}
-            <section className="Login-box"> {/* For login box rendering */}
-                    <h2 className="title">Welcome Back</h2>
-                    <p className="top-text">Sign in to continue your journey</p>
-
-                {/* form calls validateForm on submit, and sets email, password variables with user entered value
-                * Outputs error msg upon form submission if any field is invalid */}
-                <form className="login-form"> 
-                    <label htmlFor="Email">Email</label><br/>
-                    <input type="email" 
-                        id="Email" 
-                        placeholder="Enter your email"
-                        onChange={ (e) => setEmail(e.target.value)}
-                        value={email}
-                        required
-                    />
-                    {emailError && <div className="error-msg">{emailError}</div>}
-
-                    <label htmlFor="Password">Password</label><br/>
-                    <input type="password" 
-                        id="password" 
-                        placeholder="Enter your password"
-                        onChange={ (e) => setPassword(e.target.value)}
-                        value={password}
-                        required
-                    />
-                    {pwdError && <div className="error-msg">{pwdError}</div>}
-                    <br />
-
-                    <button type="submit" className="login-button">Login</button><br />
-                    <Link to="/register" className="register"> 
-                        Don't have an account? Sign up {/* Links to register page on click*/}
-                    </Link>
-                </form>
-            </section>
+        <div>
+            <h1>Login</h1>
+            <p>hello im just a placeholder</p>
         </div>
     )
 }
