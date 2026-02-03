a2226df30e2c2b902de46268a8d30ca146e82ab7
qinkathe <katherine.qin@mail.utoronto.ca>
2026-02-02 21:49:51 -0500

    FE-7 added form components of login page

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-            <p>hello im just a placeholder</p>
+        <div className="Login-layout"> {/* For login box layout */}
+            {loginMsg && <div className="login-msg">{loginMsg}</div>}
+            <section className="Login-box"> {/* For login box rendering */}
+                    <h2 className="title">Welcome Back</h2>
+                    <p className="top-text">Sign in to continue your journey</p>
+
+                {/* form calls validateForm on submit, and sets email, password variables with user entered value
+                * Outputs error msg upon form submission if any field is invalid */}
+                <form onSubmit={validateForm} className="login-form"> 
+                    <label htmlFor="Email">Email</label><br/>
+                    <input type="email" 
+                        id="Email" 
+                        placeholder="Enter your email"
+                        onChange={ (e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                    />
+                    {emailError && <div className="error-msg">{emailError}</div>}
+
+                    <label htmlFor="Password">Password</label><br/>
+                    <input type="password" 
+                        id="password" 
+                        placeholder="Enter your password"
+                        onChange={ (e) => setPassword(e.target.value)}
+                        value={password}
+                        required
+                    />
+                    {pwdError && <div className="error-msg">{pwdError}</div>}
+                    <br />
+
+                    <button type="submit" className="login-button">Login</button><br />
+                    <Link to="/register" className="register"> 
+                        Don't have an account? Sign up {/* Links to register page on click*/}
+                    </Link>
+                </form>
+            </section>
         </div>
     )
 }
