706d82ca02df1ee46dfa9345c5ce403725e90b0f
aqzwsxe <fzh200393@gmail.com>
2026-02-03 22:11:08 -0500

    1: Create the User class; 2: created the AuthenticationController; 3: create the database scheme

---
+++ backend/model/User.py
@@ -0,0 +1,52 @@
+import psycopg2
+import os
+
+def get_db_connection():
+    return psycopg2.connect(host=os.getenv("DB_HOST", "localhost"), 
+                            database=os.getenv("DB_NAME", "teamify_db"),
+                            user=os.getenv("DB_USER", "postgres"),
+                            password=os.getenv("DB_PASS")
+                            )
+
+
+class User:
+    @staticmethod
+    def create1(username, email, password_hash, token):
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO users (username,email, password_hash, verification_token) VALUES (%s, %s, %s, %s) RETURNING id;",
+            (username, email, password_hash, token)
+        )
+
+        user_id = cur.fetchone()[0]
+        conn.commit()
+        cur.close()
+        conn.close()
+        return user_id 
+    
+
+
+    @staticmethod
+    def find_by_email(email):
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT id, username, password_hash, is_verified FROM users WHERE email = %s;", (email,))
+        user1 = cur.fetchone()
+        cur.close()
+        conn.close()
+        return user1 
+    
+
+    @staticmethod
+    def verify1(token):
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("UPDATE users SET is_verified = TRUE, verification_token=NULL WHERE verification_token = %s RETURNING id;", (token,))
+        updated_user = cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
+        return updated_user
+
+
---
+++ backend/model/database_scheme.sql
@@ -0,0 +1,36 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    my_games_list TEXT,
+    profile_picture_url TEXT,
+    bio VARCHAR(500),
+    sub_class VARCHAR(50),
+    is_verified BOOLEAN DEFAULT FALSE,
+    verification_token VARCHAR(255)
+);
+
+
+
+-- picture
+-- bio descruption
+-- sub-class
+-- for profile page
+
+
+-- class for games
+
+CREATE TABLE parties (
+    id SERIAL PRIMARY KEY,
+    leader_id INT REFERENCES users(id) ON DELETE CASCADE,
+    game_mode VARCHAR(50) NOT NULL, 
+    rank_requirement VARCHAR(50),
+    region VARCHAR(50) NOT NULL,
+    current_players INT DEFAULT 1,
+    max_players INT NOT NULL,
+    CONSTRAINT player_limit CHECK (current_players <= max_players)
+    CONSTRAINT min_players CHECK (max_players > 0)
+);
+
