dbfa72d9917f783d4ec4ff52217ab69fa5df8b31
qinkathe <katherine.qin@mail.utoronto.ca>
2026-02-04 01:18:25 -0500

    FE-2 Implemented UI for register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route} from "react-router-dom"
 import Login from "./routes/Login"
 import Profile from "./routes/Profile"
 import ProfileEdit from './routes/ProfileEdit.jsx'
+import Register from "./routes/Register.jsx"
 import { useState } from "react"
 
 // npm run dev http://localhost:5173/ 
@@ -20,6 +21,7 @@ function App() {
             <Route path="/login" element={<Login setUser={setUser}/>} />
             <Route path="/profile" element={<Profile/>} />
             <Route path="/profile_editing" element={<ProfileEdit/>} />
+            <Route path="/register" element={<Register/>} />
           </Routes>
       </div>
     </>
--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -4,21 +4,24 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
-const Login = ( { setUser }) => {
+const Register = ( ) => {
+    const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [usernameError, setUsernameError] = useState("")
     const [emailError, setEmailError] = useState("")
     const [pwdError, setPwdError] = useState("")
-    const [loginMsg, setloginMsg] = useState("")
+    const [registerMsg, setRegisterMsg] = useState("")
     const navigate = useNavigate()
 
     // If user modifies email or password, empty error msg
     useEffect(() => {
+        setUsernameError('')
         setEmailError('')
         setPwdError('')
-    }, [email, password])
+    }, [username, email, password])
 
-    // validates the form components and output error msg if needed
+    // validates the form components and output error msg if needed 
     const validateForm = async (e) => {
         e.preventDefault() //Prevents default behaviour from reloading the page 
 
@@ -27,8 +30,18 @@ const Login = ( { setUser }) => {
         setEmailError("")
         setPwdError("")
 
+        // TODO: check if email already exists
+
+        // This regex for a valid username is found online
+        // output error if username is empty, includes spaces, special characters, <3 letters, or >20 letters
+        const usernameRegex = /^[A-Za-z0-9]{3,20}$/
+        if (username === "" || !usernameRegex.test(username)) {
+            setUsernameError("Username cannot include spaces, special characters, and must be 3-20 letters")
+            valid = false
+        }
+
         // This regex for a valid email is found online
-        // output error if email is empty or does not fit the email format
+        // output error if email is empty, or does not fit the email format
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         if (email === "" || !emailRegex.test(email)) {
             setEmailError("Email empty or incorrect format")
@@ -36,47 +49,55 @@ const Login = ( { setUser }) => {
         }
         
         // This regex for a valid password is found online
-        // output error if the password is empty or does not have at least one letter, one number, and min 8 letters
+        // output error if the password is empty, does not have at least one letter, at least one number, and min 8 letters
         const passRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/
         if (password == "" || !passRegex.test(password)) {
-            setPwdError("Invalid password")
+            setPwdError("Password must include at least one letter, one number, and minimum 8 letters")
             valid = false
         }
 
         if (!valid) return
 
-        // call backend api to check if user exists
-        let success = await onLoginSubmit(email, password)
+        // call backend api to check if registeration successful
+        let success = await onRegisterSubmit(username, email, password)
 
         if (success) {
-            console.log("login success!")
-            navigate("/profile") // redirect to profile page for now, until matchmaking gets implemented
+            console.log("Register success!")
+            navigate("/login") //redirect to login page
         }else {
-            setloginMsg("Invalid username or password")
-            console.log("login failed!")
+            setRegisterMsg("Register failed, please try again.")
+            console.log("Registration failed!")
         }
     }
 
-    // function used to fetch and get response from database to check if user exists
-    // Return true if user exists, and false otherwise
-    const onLoginSubmit = async (email, password) => {
-        // TODO: call database with email and password
-        // set the username with the data received from the database
+    // send registeration data and get response from db to check if registeration is successful
+    // Return true if user registration succeeded, else false
+    const onRegisterSubmit = async (username, email, password) => {
+        // TODO: send username, email and password to database
 
-        setUser("user") // placeholder
         return true
     } 
 
     return (
         <div className="Login-layout"> {/* For login box layout */}
-            {loginMsg && <div className="login-msg">{loginMsg}</div>}
+            {registerMsg && <div className="login-msg">{registerMsg}</div>}
             <section className="Login-box"> {/* For login box rendering */}
-                    <h2 className="title">Welcome Back</h2>
-                    <p className="top-text">Sign in to continue your journey</p>
+                    <h2 className="title">Join the Family</h2>
+                    <p className="top-text">Register to meet your gaming crew</p>
 
                 {/* form calls validateForm on submit, and sets email, password variables with user entered value
                 * Outputs error msg upon form submission if any field is invalid */}
                 <form onSubmit={validateForm} className="login-form"> 
+                    <label htmlFor="Username">Username</label><br/>
+                    <input type="text" 
+                        id="Username" 
+                        placeholder="Enter an username"
+                        onChange={ (e) => setUsername(e.target.value)}
+                        value={username}
+                        required
+                    />
+                    {usernameError && <div className="error-msg">{usernameError}</div>}
+                    
                     <label htmlFor="Email">Email</label><br/>
                     <input type="email" 
                         id="Email" 
@@ -98,9 +119,9 @@ const Login = ( { setUser }) => {
                     {pwdError && <div className="error-msg">{pwdError}</div>}
                     <br />
 
-                    <button type="submit" className="login-button">Login</button><br />
-                    <Link to="/register" className="register"> 
-                        Don't have an account? Sign up {/* Links to register page on click*/}
+                    <button type="submit" className="login-button">Join</button><br />
+                    <Link to="/login" className="register"> {/* className kept as register for Login.css */}
+                        Already have an account? Sign in {/* Links to login page on click */}
                     </Link>
                 </form>
             </section>
@@ -108,4 +129,4 @@ const Login = ( { setUser }) => {
     )
 }
 
-export default Login
+export default Register
