7d34d2ac5eda398a80af9a5a1d045950f2fc83b1
Daniel Steven <dan.p.steven@gmail.com>
2026-02-21 00:33:19 -0500

    add updated user class
    * update user class with better python representation
    * integrate psycopg2 with python to create User objects

---
+++ backend/model/user.py
@@ -0,0 +1,118 @@
+import psycopg2
+
+
+DB_CONFIG = {
+    "dbname": "teamify_db",
+    "user": "postgres",
+    "password": "password",
+    "host": "localhost",
+    "port": 5432
+}
+
+
+def get_db_connection():
+    return psycopg2.connect(**DB_CONFIG)
+
+class User:
+    def __init__(self, id, username, email, description=None, pfp_url=None):
+
+        self.id = id
+        self.username = username
+        self.email = email
+        self.description = description
+        self.pfp_url = pfp_url
+
+    @staticmethod
+    def create(username, email, hashed_password):
+
+        conn = get_db_connection()
+        try:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO users (username, email, password_hash)
+                    VALUES (%s, %s, %s)
+                    RETURNING id, username, email
+                    """,
+                    (username, email, hashed_password)
+                )
+                row = cur.fetchone()
+            conn.commit()
+            return User(*row)
+        except Exception as e:
+            conn.rollback()
+            raise e
+        finally:
+            conn.close()
+
+    @staticmethod
+    def find_by_id(user_id):
+        '''
+        Find and return a User object based on an user id.
+
+        Args:
+            id: int.
+        Returns:
+            User object pertaining to user id.
+        '''
+        conn = get_db_connection()
+        try:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT id, username, email, description, profile_picture_url FROM users WHERE id = %s",
+                    (user_id,)
+                )
+                row = cur.fetchone()
+                if row:
+                    return User(*row)
+                return None
+        finally:
+            conn.close()
+
+    @staticmethod
+    def find_by_email(email):
+        '''
+        Find and return a User object based on an email.
+
+        Args:
+            email: string.
+        Returns:
+            User object pertaining to registered email.
+        '''
+        conn = get_db_connection()
+        try:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT id, username, email, description, profile_picture_url FROM users WHERE email = %s",
+                    (email,)
+                )
+                row = cur.fetchone()
+                if row:
+                    return User(*row)
+                return None
+        finally:
+            conn.close()
+
+    def to_dict(self):
+        """
+        Return a dictionary representation of the User object.
+
+        Returns:
+            Dictionary representation of user object.
+
+        """
+
+        return {
+                "id": self.id,
+                "username": self.username,
+                "email": self.email,
+                "description": self.description,
+                "pfp_url": self.pfp_url
+                }
+
+'''
+username
+old email new email
+old password new password
+description
+'''
