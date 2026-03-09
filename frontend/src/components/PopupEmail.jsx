04291a688a91e8741592e9654e244bdb42f24790
lurenhong <haoranhong666@gmail.com>
2026-03-09 16:48:53 -0400

    Fixed code spacing

--- frontend/src/components/PopupEmail.jsx
+++ frontend/src/components/PopupEmail.jsx
@@ -3,134 +3,133 @@ import "./PopupEmail.css";
 import Popup from "./Popup"
 
 export default function PUEmail({email, emailModifier}) {
-  const [open, setOpen] = useState(false);
-  const [oldEmail, setOldEmail] = useState("");
-  const [newEmail, setNewEmail] = useState("");
-  const [isSaving, setIsSaving] = useState(false);
-  const [oldEmailError, setOldEmailError] = useState("");
-  const [newEmailError, setNewEmailError] = useState("");
-  const [emailFail, setFail] = useState("");
-  async function handleSave() {
-    setFail("");
-    let isValid = true;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    if (oldEmail === "" || !emailRegex.test(oldEmail)) {
-      setOldEmailError("Email empty or incorrect format");
-      isValid = false;
-    } else {
-      setOldEmailError("");
-    }
-    if (newEmail === "" || !emailRegex.test(newEmail)) {
-      setNewEmailError("Email empty or incorrect format");
-      isValid = false;
-    } else {
-      setNewEmailError("");
-    }
-    if (!isValid) {
-      return;
-    }
-    
-    if (oldEmail === newEmail) {
-      setNewEmailError("New email must be different from your current email.");
-      return;
-    } else {
-      setNewEmailError("");
-    }
-
-    setIsSaving(true);
-
-    try {
-      const res = await fetch("/api/user/me", {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("access_token")}` },
-        body: JSON.stringify({ old_email: oldEmail, new_email: newEmail }),
-      });
-
-      if (!res.ok) {
-        // backend might return JSON error { message: "..." }
-        setFail(`Save failed. Please try again later. (${res.status})`);
-        let msg = "";
-        try {
-          const data = await res.json();
-          console.log(data);
-          if (data?.status) msg = data.status;
-          setFail(`Save failed. Please try again later. (${msg})`);
-        } catch {}
-        throw new Error(msg);
-      }
-
-      handleClose();
-      emailModifier(newEmail);
-    } catch (e) {
-      // setFail(e instanceof Error ? e.message : "Save failed.");
-      setFail(`Save failed. Please try again later. (${e.message})`);
-    } finally {
-      setIsSaving(false);
-    }
-  };
-  function handleClose() {
-    setOldEmail("");
-    setNewEmail("");
-    setIsSaving(false);
-    setOpen(false);
-    setOldEmailError("");
-    setNewEmailError("");
-    setFail("");
-  };
-
-  return (
-    <div className="element-email">
-      <button className="btn-email btn-email-change" onClick={() => setOpen(true)}>
-        Change Email
-      </button>
-
-      <Popup open={open} onClose={handleClose} title="Change Email" fail_msg={emailFail} 
-        body_height="popup-email-body-height" popup_width="popup-email-popup-width">
-        <div style={{ display: "flex", gap: 8 }}>
-          <div className="email-input-container">
-            <div className="single-input-block-email"><h2>Enter your current email</h2>
-              <div style={{ display: "flex", gap: 8 }}>
-                <input
-                  className="input-email"
-                  placeholder="Type here..."
-                  value={oldEmail}
-                  onChange={(e) => setOldEmail(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") handleSave();
-                  }}
-                />
-              </div>
-              {oldEmailError ? <p className="error-msg">{oldEmailError}</p> : ""}
-            </div>
-
-            <div className="single-input-block-email"><h2>Enter your new email</h2>
-              <div style={{ display: "flex", gap: 8 }}>
-                <input
-                  className="input-email"
-                  placeholder="Type here..."
-                  value={newEmail}
-                  onChange={(e) => setNewEmail(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") handleSave();
-                  }}
-                />
-              </div>
-              {newEmailError ? <p className="error-msg">{newEmailError}</p> : ""}
-            </div>
-          </div>
-          <div className="email-save-container">
-            <button
-              className="btn-email btn-email-save"
-              onClick={handleSave}
-              disabled={isSaving}
-              type="button"
-            >
-              {isSaving ? "Saving..." : "Save"}
-            </button>
-          </div>
-        </div>
+    const [open, setOpen] = useState(false);
+    const [oldEmail, setOldEmail] = useState("");
+    const [newEmail, setNewEmail] = useState("");
+    const [isSaving, setIsSaving] = useState(false);
+    const [oldEmailError, setOldEmailError] = useState("");
+    const [newEmailError, setNewEmailError] = useState("");
+    const [emailFail, setFail] = useState("");
+    async function handleSave() {
+        setFail("");
+        let isValid = true;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if (oldEmail === "" || !emailRegex.test(oldEmail)) {
+            setOldEmailError("Email empty or incorrect format");
+            isValid = false;
+        } else {
+            setOldEmailError("");
+        }
+        if (newEmail === "" || !emailRegex.test(newEmail)) {
+            setNewEmailError("Email empty or incorrect format");
+            isValid = false;
+        } else {
+            setNewEmailError("");
+        }
+        if (!isValid) {
+            return;
+        }
         
-      </Popup>
-    </div>
-  );
+        if (oldEmail === newEmail) {
+            setNewEmailError("New email must be different from your current email.");
+            return;
+        } else {
+            setNewEmailError("");
+        }
+
+        setIsSaving(true);
+
+        try {
+            const res = await fetch("/api/user/me", {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("access_token")}` },
+                body: JSON.stringify({ old_email: oldEmail, new_email: newEmail }),
+            });
+
+            if (!res.ok) {
+                // backend might return JSON error { message: "..." }
+                setFail(`Save failed. Please try again later. (${res.status})`);
+                let msg = "";
+                try {
+                    const data = await res.json();
+                    console.log(data);
+                    if (data?.status) msg = data.status;
+                    setFail(`Save failed. Please try again later. (${msg})`);
+                } catch {}
+                throw new Error(msg);
+            }
+
+            handleClose();
+            emailModifier(newEmail);
+        } catch (e) {
+            // setFail(e instanceof Error ? e.message : "Save failed.");
+            setFail(`Save failed. Please try again later. (${e.message})`);
+        } finally {
+            setIsSaving(false);
+        }
+    };
+    function handleClose() {
+        setOldEmail("");
+        setNewEmail("");
+        setIsSaving(false);
+        setOpen(false);
+        setOldEmailError("");
+        setNewEmailError("");
+        setFail("");
+    };
+
+    return (
+        <div className="element-email">
+          <button className="btn-email btn-email-change" onClick={() => setOpen(true)}>
+            Change Email
+          </button>
+
+          <Popup open={open} onClose={handleClose} title="Change Email" fail_msg={emailFail} 
+              body_height="popup-email-body-height" popup_width="popup-email-popup-width">
+              <div style={{ display: "flex", gap: 8 }}>
+                  <div className="email-input-container">
+                      <div className="single-input-block-email"><h2>Enter your current email</h2>
+                          <div style={{ display: "flex", gap: 8 }}>
+                              <input
+                                  className="input-email"
+                                  placeholder="Type here..."
+                                  value={oldEmail}
+                                  onChange={(e) => setOldEmail(e.target.value)}
+                                  onKeyDown={(e) => {
+                                    if (e.key === "Enter") handleSave();
+                                  }}
+                              />
+                          </div>
+                          {oldEmailError ? <p className="error-msg">{oldEmailError}</p> : ""}
+                      </div>
+
+                      <div className="single-input-block-email"><h2>Enter your new email</h2>
+                          <div style={{ display: "flex", gap: 8 }}>
+                              <input
+                                  className="input-email"
+                                  placeholder="Type here..."
+                                  value={newEmail}
+                                  onChange={(e) => setNewEmail(e.target.value)}
+                                  onKeyDown={(e) => {
+                                    if (e.key === "Enter") handleSave();
+                                  }}
+                              />
+                          </div>
+                          {newEmailError ? <p className="error-msg">{newEmailError}</p> : ""}
+                        </div>
+                    </div>
+                    <div className="email-save-container">
+                        <button
+                            className="btn-email btn-email-save"
+                            onClick={handleSave}
+                            disabled={isSaving}
+                            type="button"
+                        >
+                            {isSaving ? "Saving..." : "Save"}
+                        </button>
+                    </div>
+                </div>
+            </Popup>
+        </div>
+    );
 }
--- frontend/src/components/PopupPassword.jsx
+++ frontend/src/components/PopupPassword.jsx
@@ -3,172 +3,172 @@ import "./PopupPassword.css";
 import Popup from "./Popup"
 
 export default function PUPassword() {
-  const [open, setOpen] = useState(false);
-  const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [cfmPassword, setCfmPassword] = useState("");
-  const [isSaving, setIsSaving] = useState(false);
-  const [oldPasswordError, setOldPasswordError] = useState("");
-  const [newPasswordError, setNewPasswordError] = useState("");
-  const [cfmPasswordError, setCfmPasswordError] = useState("");
-  const [passwordFail, setFail] = useState("");
-  async function handleSave() {
-    setFail("");
-    let isValid = true;
-    if (oldPassword == "") {
-      setOldPasswordError("This field cannot be empty");
-      isValid = false;
-    } else {
-      setOldPasswordError("");
-    }
-    const passRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/
-    if (newPassword == "" || !passRegex.test(newPassword)) {
-      setNewPasswordError("Password must include at least one letter, one number, and minimum 8 letters");
-      isValid = false;
-    } else {
-      setNewPasswordError("");
-    }
-    if (cfmPassword === "" || !passRegex.test(cfmPassword)) {
-      setCfmPasswordError("Password must include at least one letter, one number, and minimum 8 letters");
-      isValid = false;
-    } else {
-      setCfmPasswordError("");
-    }
+    const [open, setOpen] = useState(false);
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [cfmPassword, setCfmPassword] = useState("");
+    const [isSaving, setIsSaving] = useState(false);
+    const [oldPasswordError, setOldPasswordError] = useState("");
+    const [newPasswordError, setNewPasswordError] = useState("");
+    const [cfmPasswordError, setCfmPasswordError] = useState("");
+    const [passwordFail, setFail] = useState("");
+    async function handleSave() {
+        setFail("");
+        let isValid = true;
+        if (oldPassword == "") {
+            setOldPasswordError("This field cannot be empty");
+            isValid = false;
+        } else {
+            setOldPasswordError("");
+        }
+        const passRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/
+        if (newPassword == "" || !passRegex.test(newPassword)) {
+            setNewPasswordError("Password must include at least one letter, one number, and minimum 8 letters");
+            isValid = false;
+        } else {
+            setNewPasswordError("");
+        }
+        if (cfmPassword === "" || !passRegex.test(cfmPassword)) {
+            setCfmPasswordError("Password must include at least one letter, one number, and minimum 8 letters");
+            isValid = false;
+        } else {
+            setCfmPasswordError("");
+        }
 
-    if (!isValid) {
-      return;
-    }
-    
-    if (oldPassword === newPassword) {
-      setNewPasswordError("New password must be different from your current password.");
-      isValid = false;
-    } else {
-      setNewPasswordError("");
-    }
-    if (newPassword != cfmPassword) {
-      setCfmPasswordError("This field must match your new password");
-      isValid = false;
-    } else {
-      setCfmPasswordError("");
-    }
+        if (!isValid) {
+            return;
+        }
+        
+        if (oldPassword === newPassword) {
+            setNewPasswordError("New password must be different from your current password.");
+            isValid = false;
+        } else {
+            setNewPasswordError("");
+        }
+        if (newPassword != cfmPassword) {
+            setCfmPasswordError("This field must match your new password");
+            isValid = false;
+        } else {
+            setCfmPasswordError("");
+        }
 
-    if (!isValid) {
-      return;
-    }
+        if (!isValid) {
+            return;
+        }
 
-    setIsSaving(true);
+        setIsSaving(true);
 
-    try {
-      const res = await fetch("/api/user/me", {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("access_token")}` },
-        body: JSON.stringify({ old_password: oldPassword, new_password: newPassword }),
-      });
-
-      if (!res.ok) {
-        // backend might return JSON error { message: "..." }
-        setFail(`Save failed. Please try again later. (${res.status})`);
-        let msg = "";
         try {
-          const data = await res.json();
-          console.log(data);
-          if (data?.status) msg = data.status;
-          setFail(`Save failed. Please try again later. (${msg})`);
-        } catch {}
-        throw new Error(msg);
-      }
+            const res = await fetch("/api/user/me", {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("access_token")}` },
+                body: JSON.stringify({ old_password: oldPassword, new_password: newPassword }),
+            });
 
-      handleClose();
-    } catch (e) {
-      // setFail(e instanceof Error ? e.message : "Save failed.");
-      setFail(`Save failed. Please try again later. (${e.message})`);
-    } finally {
-      setIsSaving(false);
-    }
-  };
-  function handleClose() {
-    setOldPassword("");
-    setNewPassword("");
-    setCfmPassword("");
-    setIsSaving(false);
-    setOpen(false);
-    setOldPasswordError("");
-    setNewPasswordError("");
-    setCfmPasswordError("");
-    setFail("");
-  };
+            if (!res.ok) {
+                // backend might return JSON error { message: "..." }
+                setFail(`Save failed. Please try again later. (${res.status})`);
+                let msg = "";
+                try {
+                    const data = await res.json();
+                    console.log(data);
+                    if (data?.status) msg = data.status;
+                    setFail(`Save failed. Please try again later. (${msg})`);
+                } catch {}
+                throw new Error(msg);
+            }
 
-  return (
-    <div className="element-pwd">
-      <button className="btn-pwd btn-pwd-change" onClick={() => setOpen(true)}>
-        Change Password
-      </button>
+            handleClose();
+        } catch (e) {
+            // setFail(e instanceof Error ? e.message : "Save failed.");
+            setFail(`Save failed. Please try again later. (${e.message})`);
+        } finally {
+            setIsSaving(false);
+        }
+    };
+    function handleClose() {
+        setOldPassword("");
+        setNewPassword("");
+        setCfmPassword("");
+        setIsSaving(false);
+        setOpen(false);
+        setOldPasswordError("");
+        setNewPasswordError("");
+        setCfmPasswordError("");
+        setFail("");
+    };
 
-      <Popup open={open} onClose={handleClose} title="Change Password" fail_msg={passwordFail} 
-                body_height="popup-pwd-body-height" popup_width="popup-pwd-popup-width">
-        <div style={{ display: "flex", gap: 8 }}>
-          <div className="pwd-input-container">
-            <div className="single-input-block-pwd">
-              <h2>Enter your current password</h2>
-              <div style={{ display: "flex", gap: 8 }}>
-                <input
-                  className="input-pwd"
-                  placeholder="Type here..."
-                  value={oldPassword}
-                  onChange={(e) => setOldPassword(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") handleSave();
-                  }}
-                />
-              </div>
-              {oldPasswordError ? <p className="error-msg">{oldPasswordError}</p> : ""}
-            </div>
-
-            <div className="single-input-block-pwd">
-              <h2>Enter your new password</h2>
-              <div style={{ display: "flex", gap: 8 }}>
-                <input
-                  className="input-pwd"
-                  placeholder="Type here..."
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") handleSave();
-                  }}
-                />
-              </div>
-              {newPasswordError ? <p className="error-msg">{newPasswordError}</p> : ""}
-            </div>
-
-            <div className="single-input-block-pwd">
-              <h2>Confirm your new password</h2>
-              <div style={{ display: "flex", gap: 8 }}>
-                <input
-                  className="input-pwd"
-                  placeholder="Type here..."
-                  value={cfmPassword}
-                  onChange={(e) => setCfmPassword(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") handleSave();
-                  }}
-                />
-              </div>
-              {cfmPasswordError ? <p className="error-msg">{cfmPasswordError}</p> : ""}
-            </div>
-          </div>
-
-          <div className="pwd-save-container">
-            <button
-              className="btn-pwd btn-pwd-save"
-              onClick={handleSave}
-              disabled={isSaving}
-              type="button"
-            >
-              {isSaving ? "Saving..." : "Save"}
+    return (
+        <div className="element-pwd">
+            <button className="btn-pwd btn-pwd-change" onClick={() => setOpen(true)}>
+                Change Password
             </button>
-          </div>
+
+            <Popup open={open} onClose={handleClose} title="Change Password" fail_msg={passwordFail} 
+                body_height="popup-pwd-body-height" popup_width="popup-pwd-popup-width">
+                <div style={{ display: "flex", gap: 8 }}>
+                    <div className="pwd-input-container">
+                        <div className="single-input-block-pwd">
+                            <h2>Enter your current password</h2>
+                            <div style={{ display: "flex", gap: 8 }}>
+                                <input
+                                    className="input-pwd"
+                                    placeholder="Type here..."
+                                    value={oldPassword}
+                                    onChange={(e) => setOldPassword(e.target.value)}
+                                    onKeyDown={(e) => {
+                                        if (e.key === "Enter") handleSave();
+                                    }}
+                                />
+                            </div>
+                            {oldPasswordError ? <p className="error-msg">{oldPasswordError}</p> : ""}
+                        </div>
+
+                        <div className="single-input-block-pwd">
+                            <h2>Enter your new password</h2>
+                            <div style={{ display: "flex", gap: 8 }}>
+                                <input
+                                    className="input-pwd"
+                                    placeholder="Type here..."
+                                    value={newPassword}
+                                    onChange={(e) => setNewPassword(e.target.value)}
+                                    onKeyDown={(e) => {
+                                        if (e.key === "Enter") handleSave();
+                                    }}
+                                />
+                            </div>
+                            {newPasswordError ? <p className="error-msg">{newPasswordError}</p> : ""}
+                        </div>
+
+                        <div className="single-input-block-pwd">
+                            <h2>Confirm your new password</h2>
+                            <div style={{ display: "flex", gap: 8 }}>
+                                <input
+                                    className="input-pwd"
+                                    placeholder="Type here..."
+                                    value={cfmPassword}
+                                    onChange={(e) => setCfmPassword(e.target.value)}
+                                    onKeyDown={(e) => {
+                                        if (e.key === "Enter") handleSave();
+                                    }}
+                                />
+                            </div>
+                            {cfmPasswordError ? <p className="error-msg">{cfmPasswordError}</p> : ""}
+                        </div>
+                    </div>
+
+                    <div className="pwd-save-container">
+                        <button
+                            className="btn-pwd btn-pwd-save"
+                            onClick={handleSave}
+                            disabled={isSaving}
+                            type="button"
+                        >
+                            {isSaving ? "Saving..." : "Save"}
+                        </button>
+                    </div>
+                </div>
+            </Popup>
         </div>
-      </Popup>
-    </div>
-  );
+    );
 }
