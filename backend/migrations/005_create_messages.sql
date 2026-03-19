1c201418682e717da6c0d2b715b53cdb0625c026
Daniel Steven <dan.p.steven@gmail.com>
2026-03-19 18:48:19 -0400

    add extra fields to messages table
    * is_edited bool
    * deleted_at timestamp
    * can be used to delete/modify messages

--- backend/migrations/005_create_messages.sql
+++ backend/migrations/005_create_messages.sql
@@ -3,10 +3,14 @@ CREATE TABLE messages (
     conversation_id INTEGER NOT NULL REFERENCES conversations(id) ON DELETE CASCADE,
     sender_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     content TEXT NOT NULL,
+
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    
+    deleted_at TIMESTAMP WITH TIME ZONE NULL,
+
     -- Highly Recommended Addition: Tracks if the recipient has seen it
-    is_read BOOLEAN DEFAULT FALSE 
+    is_read BOOLEAN DEFAULT FALSE,
+    is_edited BOOLEAN DEFAULT FALSE,
+
 );
 
 -- Index to quickly fetch the history of a specific chat room
