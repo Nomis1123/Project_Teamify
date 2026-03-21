5583e619ce3af234b8cf03011802ea48fd592fc3
zhengci1 <cindyxp.zheng@mail.utoronto.ca>
2026-03-21 17:42:23 -0400

    Cleaned up comments

--- backend/controller/SteamService.py
+++ backend/controller/SteamService.py
@@ -4,11 +4,6 @@
 # More info can be found here:
 # https://developer.valvesoftware.com/wiki/Steam_Web_API
 
-# NOTE FOR DEVELOPERS:
-# There are many other things we can get from the API calls please see the above url.
-# If there is anything we think is good to add it should be easy to do once we get one working.
-# Ex. GetPlayerSummaries
-
 import os
 import requests
 from dotenv import load_dotenv
@@ -21,7 +16,6 @@ STEAM_OWNED_GAMES_URL = "https://api.steampowered.com/IPlayerService/GetOwnedGam
 
 # Returns a list of games a player owns
 def get_owned_games(steam_id: str):
-    # FOR WHOEVER CALLS THIS FUNCTION: if you receive this exception either recall the function or display an empty list
     if not STEAM_API_KEY:
         raise Exception("STEAM_API_KEY not set")
     
@@ -37,24 +31,19 @@ def get_owned_games(steam_id: str):
     data = response.json()
 
     # Select what information to keep and return
-    # NOTE: Please check the sample below to see if anything else should be stored
     games = []
     for game in data.get("response", {}).get("games", []):
         # Picture of the games if needed
         icon_hash = game.get("img_icon_url")
-        
-        # FRONTEND: need to account for None and having a backup image
         icon_url = None
+
         if icon_hash:
             icon_url = f"https://media.steampowered.com/steamcommunity/public/images/apps/{game['appid']}/{icon_hash}.jpg"
 
-        # BACKEND: format returned that needs to be stored with each users list of owned games or a general games table
-        # FRONTEND: if you need to use the photo just note it is saved as a jpg url
         games.append({"appid": game["appid"],
                       "name": game.get("name", "Unknown"),
-                      "playtime_forever": game.get("playtime_forever", 0)
+                      "playtime_forever": game.get("playtime_forever", 0),
                       "icon_url": icon_url})
-    # NOTE: if user account is private, we are not able to access this info, and it will return an empty list
     return games
 
 # NOTE: EXAMPLE RETURN FROM STEAM
