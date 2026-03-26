96cf4911431f1762887ac4fc7bd6d099d8abf7ba
Daniel Steven <dan.p.steven@gmail.com>
2026-03-26 17:33:28 -0400

    change migration ordering

--- backend/migrations/010_create_user_games.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_games(
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    game_id INT REFERENCES games(id) ON DELETE CASCADE,
-    ingame_name VARCHAR(100), -- Users' IGN for that specific game
-    current_rank VARCHAR(50),
-    is_main_game BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (user_id, game_id)
-);
