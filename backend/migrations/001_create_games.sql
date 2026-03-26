8ee8e2c983639e8a29f8b9f47545606ba3505b6e
Daniel Steven <dan.p.steven@gmail.com>
2026-03-26 17:31:58 -0400

    add more migrations

--- backend/migrations/001_create_games.sql
+++ backend/migrations/001_create_games.sql
@@ -1,5 +1,7 @@
 CREATE TABLE games(
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) UNIQUE NOT NULL,
-    thumbnail_url TEXT -- a string of thext that stores a link to an image file
+    genre VARCHAR(50),
+    thumbnail_url TEXT, -- a string of thext that stores a link to an image file
+    developer VARCHAR (100)
 );
