1c68a489f0926c1cc2be77ce6c8e612107b6cdfa
Daniel Steven <dan.p.steven@gmail.com>
2026-03-26 20:05:37 -0400

    update home.css to Home.css for linux users

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import "./home.css";
+import "./Home.css";
 import controllerImg from "../assets/controller.png"
 import teamify_title from "../assets/Teamify.png"
 import { useNavigate } from "react-router-dom"
